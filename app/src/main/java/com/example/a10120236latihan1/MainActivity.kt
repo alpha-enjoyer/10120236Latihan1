f4bd0daeb6d96a574d00370ea0c42bb2a14d5ea5
alpha-enjoyer <zazizan50@gmail.com>
2023-04-11 17:38:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10120236latihan1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a10120236latihan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//NIM               : 10120236
+//NAMA              : Zulfikar Azizan
+//KELAS             : IF-6
+//TANGGAL PENGERJAAN: 11 April 2023
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
